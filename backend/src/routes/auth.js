e1db9936c8b8247c38c0a0c9de290dfc80aa5177
HarshVardhan Chauhan <harshvardhanchauhan719@gmail.com>
2025-12-05 07:32:26 +0530

    comments fucntion created

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -6,5 +6,5 @@ const auth = require('../middleware/authMiddleware');
 router.post('/register', controller.register);
 router.post('/login', controller.login);
 router.get('/me', auth, controller.getMe);
-
+router.get("/comments/:videoId", controller.getComments);
 module.exports = router;
