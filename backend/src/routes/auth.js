61658fd0b58aa6ebad40d0673794b61e4ebad3e2
HarshVardhan Chauhan <harshvardhanchauhan719@gmail.com>
2025-12-05 14:47:08 +0530

    comments

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,13 @@
+// backend/src/routes/auth.js
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/authController');
 const auth = require('../middleware/authMiddleware');
+const authController = require('../controllers/authController');
+const commentsController = require('../controllers/comments');
 
-router.post('/register', controller.register);
-router.post('/login', controller.login);
-router.get('/me', auth, controller.getMe);
-router.get("/comments/:videoId", controller.getComments);
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.get('/me', auth, authController.getMe);
+router.post('/comments', commentsController.getComments);
+
 module.exports = router;
